054061c236a3ab34de127a97d400b699016686c1
FengYouJun <845225187@qq.com>
2022-11-07 20:15:22 +0800

    fix: 修复只启用doc feature的时候reqwest::multipart未定义的问题

--- src/document/mod.rs
+++ src/document/mod.rs
@@ -5,7 +5,7 @@
 //! 启动文档翻译需要开启`doc` feature
 //!
 //! ```toml
-//! baidu_trans = { version = "0.7.4", features = ["doc"] }
+//! baidu_trans = { version = "0.7.5", features = ["doc"] }
 //! ```
 
 pub mod model;
